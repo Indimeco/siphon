34c6da1aea32819df05e2d98ce63138401f72b8e
Indimeco <18373729+Indimeco@users.noreply.github.com>
2023-03-05 10:23:12 +1100

    add ability to get parse tags

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.path)?;
+    let tags = get_tags(&contents);
+    println!("{:#?}", tags);
+
+    // get all files
+    // get tags for all files
+    // filter out unpublished files
+    // create collections from published files
+
     Ok(())
 }
 
@@ -24,48 +33,113 @@ impl Config {
     }
 }
 
-fn search<'a>(query: &str, contents: &'a str, insensitive: bool) -> Vec<&'a str> {
-    contents
+fn is_published(tags: HashMap<&str, &str>) -> bool {
+    let r = match tags.get("publish") {
+        Some(v) => *v == "true",
+        None => false,
+    };
+    r
+}
+
+fn trace<T: core::fmt::Debug + core::fmt::Display>(x: T) -> T {
+    println!("{:#?}", x);
+    x
+}
+
+fn get_tags(contents: &str) -> Result<HashMap<&str, &str>, &str> {
+    let separator = "---";
+    let mut tag_indicators = contents.match_indices(separator);
+    let tag_start = match tag_indicators.next() {
+        Some(i) => i.0,
+        None => return Err("No tag indicators"),
+    };
+    if tag_start != 0 {
+        return Err("Tag start was not at the beginning of the file");
+    }
+    let tag_end = match tag_indicators.next() {
+        Some(i) => i.0,
+        None => return Err("No end indicator for tags"),
+    };
+
+    let tags = match contents.get(tag_start + separator.bytes().len()..tag_end) {
+        Some(s) => s,
+        None => return Err("No tags"),
+    };
+    trace(tags);
+
+    let lines = tags
+        .trim()
         .lines()
-        .filter(|line| {
-            if insensitive {
-                line.to_lowercase().contains(&query.to_lowercase())
-            } else {
-                line.contains(query)
-            }
-        })
-        .collect()
+        .map(|l| l.split(":").map(|v| v.trim()).collect::<Vec<&str>>())
+        // TODO this will produce unintuitive error messages
+        .map(|a| (*a.get(0).unwrap(), *a.get(1).unwrap()))
+        .collect::<HashMap<&str, &str>>();
+
+    Ok(lines)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    mod search_test {
+
+    mod get_tags {
         use super::*;
         #[test]
-        fn one_result() {
-            let query = "duct";
+        fn one_tag() {
             let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.";
-
-            assert_eq!(
-                vec!["safe, fast, productive."],
-                search(query, contents, false)
-            );
+---
+tag: duck
+---
+";
+            let result = get_tags(contents).unwrap();
+            assert_eq!(*result.get("tag").unwrap(), "duck");
         }
 
         #[test]
-        fn case_insensitive() {
-            let query = "rUsT";
+        fn two_tags() {
             let contents = "\
-Rust:
-safe, fast, productive.
-Pick three.
-Trust me.";
+---
+tag: duck
+tag2: rabbit
+---
+";
+            let result = get_tags(contents).unwrap();
+            assert_eq!(*result.get("tag").unwrap(), "duck");
+            assert_eq!(*result.get("tag2").unwrap(), "rabbit");
+        }
 
-            assert_eq!(vec!["Rust:", "Trust me."], search(query, contents, true));
+        #[test]
+        fn missing_start_tag() {
+            let contents = "\
+tag: duck
+---
+";
+            let result = get_tags(contents).err().unwrap();
+            assert_eq!(result, "Tag start was not at the beginning of the file");
+        }
+
+        #[test]
+        fn missing_end_tag() {
+            let contents = "\
+---
+tag: duck
+bloopy bolp
+";
+            let result = get_tags(contents).err().unwrap();
+            assert_eq!(result, "No end indicator for tags");
+        }
+
+        #[test]
+        fn multi_value_tags() {
+            let contents = "\
+---
+tag: duck goat sheep chicken
+tag2: rabbit
+---
+";
+            let result = get_tags(contents).unwrap();
+            assert_eq!(*result.get("tag").unwrap(), "duck goat sheep chicken");
+            assert_eq!(*result.get("tag2").unwrap(), "rabbit");
         }
     }
 }
