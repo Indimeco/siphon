79862618173e4f89a59a23244919601d1242824b
Indimeco <18373729+Indimeco@users.noreply.github.com>
2023-03-06 19:17:27 +1100

    aggregate collections

--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,36 @@
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
-use std::hash::Hash;
-use std::io;
-use std::path;
 use walkdir::WalkDir;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    // TODO cleanup
-    let path = path::Path::new(&config.path);
-    let mut collection_aggregator: HashMap<&str, Vec<&str>> = HashMap::new();
-
-    let collections: HashMap<String, String> = WalkDir::new(config.path)
+    let collections: HashMap<String, Vec<String>> = WalkDir::new(config.path)
         .into_iter()
         .filter_map(|f| {
             let a = f.unwrap();
-            // TODO skip directories
+            if a.metadata().unwrap().is_dir() {
+                return None;
+            }
             let r = fs::read_to_string(a.path()).unwrap();
             let t = get_tags(&r).unwrap();
             if is_published(&t) {
-                return Some(t);
+                let collections = parse_collections(t.get("collections").unwrap());
+                return Some((
+                    a.file_name().to_str().unwrap().to_string(),
+                    collections.clone(),
+                ));
             } else {
                 return None;
             }
         })
-        .flatten() // huh, not sure why I need this atm, was it nested?
-        .collect();
+        .fold(
+            HashMap::new(),
+            |acc: HashMap<String, Vec<String>>, (name, collections)| {
+                aggregate_collections(name, collections, acc)
+            },
+        );
 
     dbg!(collections);
-
-    // visit_dirs(path, &|entry: &fs::DirEntry| {
-    //     let path = entry.path();
-
-    //     let contents = fs::read_to_string(path.clone()).unwrap();
-    //     let tags = get_tags(&contents).unwrap();
-    //     if is_published(&tags) {
-    //         let collections = tags.get("collections");
-    //         match collections {
-    //             Some(c) => {
-    //                 let parsed_collections = parse_collections(c);
-    //                 let copy_shit = collection_aggregator.clone();
-
-    //                 collection_aggregator =
-    //                     aggregate_collections("horseshit", parsed_collections, copy_shit);
-    //                 ()
-    //             }
-    //             None => (),
-    //         };
-    //     }
-    // });
 
     // get all files
     // get tags for all files
@@ -56,26 +38,6 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // create collections from published files
     // write collections to new files
 
-    Ok(())
-}
-
-fn parse_collections(raw: &str) -> Vec<&str> {
-    raw.trim().split(' ').collect()
-}
-
-// taken from std::fs::read_dir docs
-fn visit_dirs(dir: &path::Path, cb: &dyn Fn(&fs::DirEntry)) -> io::Result<()> {
-    if dir.is_dir() {
-        for entry in fs::read_dir(dir)? {
-            let entry = entry?;
-            let path = entry.path();
-            if path.is_dir() {
-                visit_dirs(&path, cb)?;
-            } else {
-                cb(&entry);
-            }
-        }
-    }
     Ok(())
 }
 
@@ -95,6 +57,10 @@ impl Config {
         // TODO Read dryrun from args somehow
         Ok(Config { path, dryrun: true })
     }
+}
+
+fn parse_collections(raw: &str) -> Vec<String> {
+    raw.trim().split(' ').map(|a| a.to_string()).collect()
 }
 
 fn is_published(tags: &HashMap<String, String>) -> bool {
@@ -137,19 +103,19 @@ fn get_tags(contents: &str) -> Result<HashMap<String, String>, &'static str> {
 }
 
 fn aggregate_collections<'a, 'b>(
-    name: &'a str,
-    collections: Vec<&'b str>,
-    mut aggregate: HashMap<&'b str, Vec<&'a str>>,
-) -> HashMap<&'b str, Vec<&'a str>> {
+    name: String,
+    collections: Vec<String>,
+    mut aggregate: HashMap<String, Vec<String>>,
+) -> HashMap<String, Vec<String>> {
     for c in collections {
-        let mut collection_to_update: Vec<&str> = match aggregate.get(c) {
+        let mut collection_to_update: Vec<String> = match aggregate.get(&c) {
             Some(existing) => existing.clone(),
             None => {
-                let something: Vec<&str> = Vec::new();
+                let something: Vec<String> = Vec::new();
                 something
             }
         };
-        collection_to_update.push(name);
+        collection_to_update.push(name.clone());
         aggregate.insert(c, collection_to_update);
     }
     aggregate
@@ -161,30 +127,46 @@ mod tests {
         use super::*;
         #[test]
         fn create_new_aggregate() {
-            let file_name_1 = "goat";
-            let file_collections_1 = vec!["animal"];
+            let file_name_1 = String::from("goat");
+            let file_collections_1 = vec![String::from("animal")];
             let result = aggregate_collections(file_name_1, file_collections_1, HashMap::new());
             assert_eq!(result.get("animal").unwrap()[0], "goat");
         }
 
         #[test]
         fn add_to_aggregate() {
-            let file_name_1 = "goat";
-            let file_collections_1 = vec!["animal"];
+            let file_name_1 = String::from("goat");
+            let file_collections_1 = vec![String::from("animal")];
             let result = aggregate_collections(file_name_1, file_collections_1, HashMap::new());
 
-            let file_name_2 = "horse";
-            let file_collections_2 = vec!["animal"];
+            let file_name_2 = String::from("horse");
+            let file_collections_2 = vec![String::from("animal")];
             let result_2 = aggregate_collections(file_name_2, file_collections_2, result);
             assert_eq!(result_2.get("animal").unwrap().len(), 2);
-            assert_eq!(result_2.get("animal").unwrap().contains(&"horse"), true);
-            assert_eq!(result_2.get("animal").unwrap().contains(&"goat"), true);
+            assert_eq!(
+                result_2
+                    .get("animal")
+                    .unwrap()
+                    .contains(&"goat".to_string()),
+                true
+            );
+            assert_eq!(
+                result_2
+                    .get("animal")
+                    .unwrap()
+                    .contains(&"horse".to_string()),
+                true
+            );
         }
 
         #[test]
         fn add_multi_collection_to_aggregate() {
-            let file_name_1 = "goat";
-            let file_collections_1 = vec!["animal", "pet", "horned"];
+            let file_name_1 = String::from("goat");
+            let file_collections_1 = vec![
+                String::from("animal"),
+                String::from("pet"),
+                String::from("horned"),
+            ];
             let result = aggregate_collections(file_name_1, file_collections_1, HashMap::new());
             assert_eq!(result.get("animal").unwrap()[0], "goat");
             assert_eq!(result.get("pet").unwrap()[0], "goat");
