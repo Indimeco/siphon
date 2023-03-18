7636db8b18893bbcc7c0c74aea936a908448d1d4
Indimeco <18373729+Indimeco@users.noreply.github.com>
2023-03-19 09:10:20 +1100

    add draft poem integration test

---
+++ tests/fixtures/draft_poem/sample.md
@@ -0,0 +1,6 @@
+---
+created: 2021-06-20
+title: sample collection
+---
+
+a description of the sample
--- tests/integration-test.rs
+++ tests/integration-test.rs
@@ -4,7 +4,6 @@ use tempfile::tempdir;
 #[test]
 fn test_write_collection() {
     let target_dir = tempdir().unwrap();
-
     let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/one_poem");
 
     let test_config = siphon::Config {
@@ -30,4 +29,45 @@ a description of the sample
     let copied_poem = fs::read_to_string(target_dir.path().join("poems/2021-05-30.md"))
         .expect("Copied poem should exist");
     assert_eq!(copied_poem, original_poem);
+}
+
+#[test]
+fn test_write_collection_without_drafts() {
+    let target_dir = tempdir().unwrap();
+    let test_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("tests/fixtures/draft_poem");
+
+    let test_config = siphon::Config {
+        target_dir: String::from(target_dir.path().to_str().unwrap()),
+        path: String::from(test_path.to_str().unwrap()),
+    };
+    let expected_output = "\
+---
+title: sample collection
+created: 2021-06-20
+poems:
+- 2021-05-30
+---
+
+a description of the sample
+";
+    siphon::run(test_config).expect("Run failed");
+    let file_output = fs::read_to_string(target_dir.path().join("collections/sample.md"))
+        .expect("Collection output file should exist");
+    assert_eq!(expected_output, file_output);
+    let copied_poem = fs::read_to_string(target_dir.path().join("poems/2021-05-30.md"))
+        .expect("Copied poem should exist");
+    assert_eq!(
+        copied_poem,
+        "\
+---
+created: 2021-05-30 17:22:18 +1000
+modified: 2021-05-30 17:36:18 +1000
+collections: sample
+publish: true
+---
+
+the real content
+
+"
+    );
 }
